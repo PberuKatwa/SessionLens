aa5c4019ee2026b30dc2ee86061f7bb93ccdb021
Pberu <pberu8531@gmail.com>
2026-02-17 16:49:55 +0300

    session setup v3:created table

--- src/repositories/sessions.repository.ts
+++ src/repositories/sessions.repository.ts
@@ -5,6 +5,16 @@ export async function createSession() {
 
     const pgPool = getPgPool();
 
+    const query = `
+      INSERT INTO sessions (id, user_id, expires_at)
+      VALUES ($1, $2, $3)
+      RETURNING id;
+    `;
+
+    const result = await pgPool.query(query);
+    const session = result.rows[0];
+
+    return session
   } catch (error) {
     throw error;
   }
