485dfb903eca41c977ca12e87df0c664033fd0f7
Pberu <pberu8531@gmail.com>
2026-02-17 10:19:10 +0300

    project setup v3:setting up jwt service

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken"
+import type { SignedUser } from "@/types/user.types"
 
 const secret = process.env.JWT_SECRET!
 
-export function signJwt(payload: object) {
-  return jwt.sign(payload, secret, { expiresIn: "7d" })
+export function signJwt(payload: SignedUser) {
+  try {
+    return jwt.sign(payload, secret, { expiresIn: "7d" })
+  } catch (error) {
+    throw error;
+  }
 }
 
 export function verifyJwt(token: string) {
