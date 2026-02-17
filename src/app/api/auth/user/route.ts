3d44e4e353e0f29541fcfa97ea89ca999177c7a9
Pberu <pberu8531@gmail.com>
2026-02-17 21:31:23 +0300

    middleware setup v3:working auth authenticator

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -5,34 +5,68 @@ import { logger } from "@/lib/logger";
 import { ProfileApiResponse, UserApiResponse } from "@/types/user.types";
 import { getSession } from "@/repositories/sessions.repository";
 import { findUserById } from "../../../../repositories/users.repository";
+import { withAuth } from "@/lib/auth.wrapper";
 
-export async function GET() {
-  try {
+export const GET = withAuth(
 
-    const cookieStore = await cookies();
-    const sessionCookie = cookieStore.get("session_id");
-    const sessionId = sessionCookie?.value;
+  async function() {
+    try {
 
-    if (!sessionId) {
-      return NextResponse.json({ error: "No session found" }, { status: 401 });
-    }
+      const cookieStore = await cookies();
+      const sessionCookie = cookieStore.get("session_id");
+      const sessionId = sessionCookie?.value;
 
-    const session = await getSession(sessionId);
-    const user = await findUserById(session.user_id);
+      if (!sessionId) {
+        return NextResponse.json({ error: "No session found" }, { status: 401 });
+      }
 
-    const response: ProfileApiResponse = {
-      success: true,
-      message: "Successfully fetched profile",
-      data:user
-    }
+      const session = await getSession(sessionId);
+      const user = await findUserById(session.user_id);
 
-    return NextResponse.json(response, { status: 200 });
-  } catch (error: any) {
+      const response: ProfileApiResponse = {
+        success: true,
+        message: "Successfully fetched profile",
+        data:user
+      }
 
-    logger.error(`Error in fetching user`, error)
-    return NextResponse.json({ message: `${error.message}` }, { status: 500 });
-  };
-}
+      return NextResponse.json(response, { status: 200 });
+    } catch (error: any) {
+
+      logger.error(`Error in fetching user`, error)
+      return NextResponse.json({ message: `${error.message}` }, { status: 500 });
+    };
+  }
+
+
+)
+
+// export async function GET() {
+//   try {
+
+//     const cookieStore = await cookies();
+//     const sessionCookie = cookieStore.get("session_id");
+//     const sessionId = sessionCookie?.value;
+
+//     if (!sessionId) {
+//       return NextResponse.json({ error: "No session found" }, { status: 401 });
+//     }
+
+//     const session = await getSession(sessionId);
+//     const user = await findUserById(session.user_id);
+
+//     const response: ProfileApiResponse = {
+//       success: true,
+//       message: "Successfully fetched profile",
+//       data:user
+//     }
+
+//     return NextResponse.json(response, { status: 200 });
+//   } catch (error: any) {
+
+//     logger.error(`Error in fetching user`, error)
+//     return NextResponse.json({ message: `${error.message}` }, { status: 500 });
+//   };
+// }
 
 export async function POST(req: NextRequest) {
   try {
---
+++ src/lib/auth.wrapper.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { getSession } from "@/repositories/sessions.repository";
+import { NextResponse } from "next/server";
+
+export function withAuth(handler: Function) {
+  return async (req: Request, ...args: any[]) => {
+    try {
+      const cookieStore = await cookies();
+      const sessionId = cookieStore.get("session_id")?.value;
+
+      if (!sessionId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+      const session = await getSession(sessionId);
+      return handler(req, session, ...args);
+    } catch (error) {
+      return NextResponse.json({ error: "Invalid Session" }, { status: 401 });
+    }
+  };
+}
