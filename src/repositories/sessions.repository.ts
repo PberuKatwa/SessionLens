22da1149143a0d7dd36c45ca36082ed5bf7a4875
Pberu <pberu8531@gmail.com>
2026-02-17 20:57:35 +0300

    Session optimization v5:single sessionlogin

--- src/repositories/sessions.repository.ts
+++ src/repositories/sessions.repository.ts
@@ -6,24 +6,22 @@ export async function createSession(userId:number) {
   try {
 
     const pgPool = getPgPool();
-
-    // const existingSession = await pgPool.query(
-    //   `SELECT `
-    // )
-
     const query = `
-      INSERT INTO sessions (user_id, expires_at)
-      VALUES ($1, $2)
+      WITH invalidate_old AS (
+        UPDATE sessions
+        SET status = 'trash'
+        WHERE user_id = $1 AND status = 'active'
+      )
+      INSERT INTO sessions (user_id, expires_at, status)
+      VALUES ($1, $2, 'active')
       RETURNING id;
     `;
 
     const expiresAt = new Date();
     expiresAt.setDate(expiresAt.getDate() + 1);
 
-    const result = await pgPool.query(query,[userId,expiresAt]);
-    const session = result.rows[0];
-
-    return session
+    const result = await pgPool.query(query, [userId, expiresAt]);
+    return result.rows[0].id;
   } catch (error) {
     throw error;
   }
@@ -37,7 +35,7 @@ export async function getSession(sessionId: string):Promise<BaseSession> {
       `
       SELECT user_id
        FROM sessions
-       WHERE id = $1
+       WHERE id = $1, status != 'trash'
        AND expires_at > NOW();
       `,
       [sessionId]
@@ -56,7 +54,7 @@ export async function trashSession(id:string) {
   try {
     const pgPool = getPgPool();
 
-    await pgPool.query(`UPDATE session SET status=$1 WHERE id=$2;`,["trash", id])
+    await pgPool.query(`UPDATE sessions SET status=$1 WHERE id=$2;`,["trash", id])
 
     logger.info("Successfully trashed session");
   } catch (error) {
