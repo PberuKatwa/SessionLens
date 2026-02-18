9f5a6b9dadb10fca79bfed9753a1703ed7687e97
Pberu <pberu8531@gmail.com>
2026-02-18 14:44:39 +0300

    group session api v2:creating session api routes

--- src/app/api/sessions/group/route.ts
+++ src/app/api/sessions/group/route.ts
@@ -1,16 +1,18 @@
 import { logger } from "@/lib/logger";
 import { ApiResponse } from "@/types/api.types";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
 async function createGroupSession(req: NextRequest) {
   try {
 
-  } catch (error:any) {
+  } catch (error: any) {
+
     logger.error(`error in creating group session`, error);
     const response: ApiResponse = {
       success: false,
       message:`${error.message}`
     }
+    return NextResponse.json(response, { status: 500 });
   }
 }
