e2f80e60eac5fd4d3467cd90ae6263c5124b0c36
Pberu <pberu8531@gmail.com>
2026-02-18 14:50:20 +0300

    group create session api v1:creating form data

--- src/app/api/sessions/group/route.ts
+++ src/app/api/sessions/group/route.ts
@@ -1,10 +1,17 @@
 import { logger } from "@/lib/logger";
 import { ApiResponse } from "@/types/api.types";
+import { BaseAuthSession } from "@/types/authSession.types";
 import { NextRequest, NextResponse } from "next/server";
 
 
-async function createGroupSession(req: NextRequest) {
+async function createGroupSession(req: NextRequest, session:BaseAuthSession) {
   try {
+
+    const formData = await req.formData();
+
+    const fellowName = formData.get("fellowName") as string;
+    const groupId = formData.get("groupId") as string;
+    const transcriptFile = formData.get("transcriptFile") as File;
 
   } catch (error: any) {
 
