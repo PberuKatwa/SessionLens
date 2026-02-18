4bbee7b605e5d231f09d9fac7874e6535f168373
Pberu <pberu8531@gmail.com>
2026-02-18 10:24:13 +0300

    session to auth session refractor

--- src/lib/auth.middleware.ts
+++ src/lib/auth.middleware.ts
@@ -1,16 +1,14 @@
 import { cookies } from "next/headers";
-import { getSession } from "@/repositories/sessions.repository";
+import { getAuthSession } from "@/repositories/sessions.repository";
 import { NextResponse } from "next/server";
+import { getCookieId } from "./cookies";
 
 export function authMiddleware(handler: Function) {
   return async (req: Request, ...args: any[]) => {
     try {
-      const cookieStore = await cookies();
-      const sessionId = cookieStore.get("session_id")?.value;
+      const { authSessionId } = await getCookieId();
 
-      if (!sessionId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-      const session = await getSession(sessionId);
+      const session = await getAuthSession(authSessionId);
       return handler(req, session, ...args);
     } catch (error) {
       return NextResponse.json({ error: "Invalid Session" }, { status: 401 });
