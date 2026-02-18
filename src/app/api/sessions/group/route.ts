4ef5d33e1244019a71b8339fa45847d3212e0745
Pberu <pberu8531@gmail.com>
2026-02-18 16:54:36 +0300

    paginated group session api v2:api

--- src/app/api/sessions/group/route.ts
+++ src/app/api/sessions/group/route.ts
@@ -1,8 +1,8 @@
 import { logger } from "@/lib/logger";
-import { createGroupSession, getGroupSessionById } from "@/repositories/groupSessions.repository";
+import { createGroupSession, getAllGroupSessions, getGroupSessionById } from "@/repositories/groupSessions.repository";
 import { ApiResponse } from "@/types/api.types";
 import { BaseAuthSession } from "@/types/authSession.types";
-import { CompleteGroupSessionApiResponse, SingleGroupSessionApiResponse } from "@/types/groupSession.types";
+import { CompleteGroupSessionApiResponse, PaginatedGroupSessionsApiResponse, SingleGroupSessionApiResponse } from "@/types/groupSession.types";
 import { GroupSessionTranscript } from "@/types/json.types";
 import { NextRequest, NextResponse } from "next/server";
 import { parseJsonFile } from "@/lib/json.manager";
@@ -45,4 +45,38 @@ async function createSession(req: NextRequest, session:BaseAuthSession) {
   }
 }
 
+async function getAllSessions(
+  req: NextRequest,
+  session: BaseAuthSession
+) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "10");
+
+    const result = await getAllGroupSessions(page, limit);
+
+    const response: PaginatedGroupSessionsApiResponse = {
+      success: true,
+      message: "Successfully fetched sessions",
+      data: result,
+    };
+
+    return NextResponse.json(response, { status: 200 });
+
+  } catch (error: any) {
+    logger.error("error in fetching group sessions", error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
+
 export const POST = authMiddleware(createSession);
+export const GET = authMiddleware(getAllSessions);
