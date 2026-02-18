d6c8f0e681905fef2f03915b3a67baa261be8455
Pberu <pberu8531@gmail.com>
2026-02-18 18:35:01 +0300

    analyzed sessions repository setup v2:created get session method

--- src/repositories/analyzedSessions.repository.ts
+++ src/repositories/analyzedSessions.repository.ts
@@ -8,9 +8,7 @@ import {
   PaginatedAnalyzedSessions
 } from "@/types/analyzedSession.types";
 
-export async function createAnalyzedSession(payload: CreateAnalyzedSessionPayload): Promise<BaseAnalyzedSession>
-
- {
+export async function createAnalyzedSession(payload: CreateAnalyzedSessionPayload): Promise<BaseAnalyzedSession>{
   try {
     const pgPool = getPgPool();
     const { session_id, is_safe, content_coverage, facilitation_quality, protocol_safety,
@@ -33,6 +31,24 @@ export async function createAnalyzedSession(payload: CreateAnalyzedSessionPayloa
     return analyzedSession;
   } catch (error) {
     logger.error("Error in createAnalyzedSession");
+    throw error;
+  }
+}
+
+export async function getAnalyzedSessionById(id: number): Promise<AnalyzedSession> {
+  try {
+    const pgPool = getPgPool();
+
+    const result = await pgPool.query(
+      `SELECT session_id, is_safe, content_coverage, facilitation_quality, protocol_safety, summary, created_at, llm_evaluation
+        FROM analyzed_sessions WHERE id = $1 AND row_status != 'trash';`,
+      [id]
+    );
+
+    if (result.rowCount === 0) throw new Error("Analyzed session not found");
+    const analyzedSession: AnalyzedSession = result.rows[0];
+    return analyzedSession;
+  } catch (error) {
     throw error;
   }
 }
